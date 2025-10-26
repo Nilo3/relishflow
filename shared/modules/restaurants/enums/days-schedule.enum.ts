ebacce78eb43784aadf0eb305d84abbb123d01a6
Gino Tapia <destroctorgt@gmail.com>
2025-10-25 19:42:54 -0500

    feat: agregar traducción de días de la semana para la gestión de horarios

--- shared/modules/restaurants/enums/days-schedule.enum.ts
+++ shared/modules/restaurants/enums/days-schedule.enum.ts
@@ -7,3 +7,13 @@ export enum DaysSchedule {
   FRIDAY = 5,
   SATURDAY = 6
 }
+
+export const DaysScheduleTranslate: Record<DaysSchedule, string> = {
+  [DaysSchedule.SUNDAY]: 'Domingo',
+  [DaysSchedule.MONDAY]: 'Lunes',
+  [DaysSchedule.TUESDAY]: 'Martes',
+  [DaysSchedule.WEDNESDAY]: 'Miércoles',
+  [DaysSchedule.THURSDAY]: 'Jueves',
+  [DaysSchedule.FRIDAY]: 'Viernes',
+  [DaysSchedule.SATURDAY]: 'Sábado'
+}
